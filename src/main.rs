a6bea8a0780e8e8174a9263cd036aa7557528ab4
byeong cheol lim <lim6112j@gmail.com>
2022-11-30 18:20:08 +0900

    await

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use mini_redis::{client, Result};
+async fn say_world() {
+    println!("world");
+}
 #[tokio::main]
-async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?;
-    client.set("hello", "world".into()).await?;
-    let result = client.get("hello").await?;
-    println!("got value from the server; result = {:?}", result);
-    Ok(())
+async fn main() {
+    let op = say_world();
+    println!("hello");
+    op.await;
 }
