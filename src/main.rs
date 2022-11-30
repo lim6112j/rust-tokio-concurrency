79a93817a6cdfd9c6128679195a3badbe1eb0cc8
byeong cheol lim <lim6112j@gmail.com>
2022-11-30 19:04:08 +0900

    concurrency

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@ async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
+        //process(socket).await;
+        tokio::spawn(async move {
+            process(socket).await;
+        });
     }
 }
 async fn process(socket: TcpStream) {
